544ea1e64e5632482b54be96037cb01fbf1a987a
Tomasz Samcik <tomek@samcik.pro>
2023-03-19 14:30:57 +0100

    Removing ioc package altogether (replaced by @Autowired)

--- src/main/java/com/altapay/backend/repositories/ShopOrderRepository.java
+++ src/main/java/com/altapay/backend/repositories/ShopOrderRepository.java
@@ -1,39 +1,32 @@
 package com.altapay.backend.repositories;
 
-import com.altapay.backend.ioc.BackendContainer;
 import com.altapay.backend.model.OrderLine;
 import com.altapay.backend.model.Product;
 import com.altapay.backend.model.ShopOrder;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ShopOrderRepository
 {
 
-    @Autowired
-    private BackendContainer backendContainer;
-
     public ShopOrder loadShopOrder( String shopOrderId )
     {
-        ShopOrder order = backendContainer.getShopOrder();
-        order.setId( shopOrderId );
-        List<OrderLine> orderLines = new ArrayList<>();
-        orderLines.add( getOrderLine( "1", "Keyboard", 1 ) );
-        order.setOrderLines( orderLines );
-        return order;
+        return ShopOrder.builder()
+            .orderLines( List.of(
+                getOrderLine( "1", "Keyboard", 1 ) ) )
+            .id( shopOrderId )
+            .build();
     }
 
     private OrderLine getOrderLine( String productId, String name, int quantity )
     {
-        OrderLine orderLine = backendContainer.getOrderLine();
-        Product product = backendContainer.getProduct();
-        product.setId( productId );
-        product.setName( name );
-        orderLine.setProduct( product );
-        orderLine.setQuantity( quantity );
-        return orderLine;
+        return OrderLine.builder()
+            .product( Product.builder()
+                .id( productId )
+                .name( name )
+                .build() )
+            .quantity( quantity )
+            .build();
     }
 
     public void saveShopOrder( ShopOrder shopOrder )
